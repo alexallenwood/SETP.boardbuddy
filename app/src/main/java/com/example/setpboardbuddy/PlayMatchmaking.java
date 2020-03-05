8f47fec55bec0803439dfde06a6fa7f2a10ecca4
alexallenwood <alexallenwood@users.noreply.github.com>
2020-03-05 22:48:45 +0000

    Layout
    Added the scroll view and buttons, alongside a spinner, titles and back button.

--- app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
+++ app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
@@ -1,8 +1,15 @@
 package com.example.setpboardbuddy;
 
+import androidx.annotation.ArrayRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
 
 public class PlayMatchmaking extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class PlayMatchmaking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_matchmaking);
+
+        Spinner mySpinner = (Spinner) findViewById(com.example.setpboardbuddy.R.id.SpinnerSort);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PlayMatchmaking.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.PlayViewSort));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
     }
+
+    public  void Back(View view){
+        Intent playMatchmaking = new Intent(PlayMatchmaking.this, MainActivity.class);
+        startActivity(playMatchmaking);
+    }
+
+
+
 }
