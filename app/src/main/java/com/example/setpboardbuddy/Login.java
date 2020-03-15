3b927d007b428494914615b7230fe9b1b0ea5829
alexallenwood <alexallenwood@users.noreply.github.com>
2020-03-15 00:00:18 +0000

    Connected to online (localhost) database
    Added DatabaseHelper, created external php scripts, verified online logins work and application can be connected to the database.

--- app/src/main/java/com/example/setpboardbuddy/Login.java
+++ app/src/main/java/com/example/setpboardbuddy/Login.java
@@ -9,14 +9,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    EditText Username, Password;
     boolean Approved = false;
-    public EditText Username;
-    public EditText Password;
+//    public EditText Username;
+//    public EditText Password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -27,19 +28,29 @@ public class Login extends AppCompatActivity {
 
     public void Login(View view) {
         Intent login = new Intent(Login.this, MainActivity.class);
-        if (Username.getText().toString().equals("test")) {
-            Password.setText("test");
-            Approved = true;
-        }
-        else {
-            CharSequence text = "User credentials not recognised.\nTry again or register as a user.";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(Login.this, text, duration);
-            toast.show();
-        }
-        if (Approved == true){
-            startActivity(login);
-        }
+//        if (Username.getText().toString().equals("test")) {
+//            Password.setText("test");
+//            Approved = true;
+
+        String username = Username.getText().toString();
+        String password = Password.getText().toString();
+        String type = "login";
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        databaseHelper.execute(type, username, password);
+
+
+
+//        }
+//        else {
+//            CharSequence text = "User credentials not recognised.\nTry again or register as a user.";
+//            int duration = Toast.LENGTH_LONG;
+//            Toast toast = Toast.makeText(Login.this, text, duration);
+//            toast.show();
+//        }
+//        if (Approved == true){
+//            startActivity(login);
+//        }
     }
 
     public void Register (View view){
