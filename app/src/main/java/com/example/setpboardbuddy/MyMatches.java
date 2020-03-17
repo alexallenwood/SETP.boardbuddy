edc22f545882faf32e4783281c5b271fea9e2810
up899044 <61065304+up899044@users.noreply.github.com>
2020-03-17 15:51:00 +0000

    Usable button

--- app/src/main/java/com/example/setpboardbuddy/MyMatches.java
+++ app/src/main/java/com/example/setpboardbuddy/MyMatches.java
@@ -2,7 +2,9 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyMatches extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MyMatches extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
+    }
+
+    public  void MainActivity(View view) {
+        Intent MainActivity = new Intent(MyMatches.this, MainActivity.class);
+        startActivity(MainActivity);
     }
 }
