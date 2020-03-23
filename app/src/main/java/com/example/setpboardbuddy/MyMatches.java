cd46e216857069aaf8371baa5867a2a8c5f88a19
up899044 <61065304+up899044@users.noreply.github.com>
2020-03-23 18:48:42 +0000

    Editing Sort By drop down attributes.

--- app/src/main/java/com/example/setpboardbuddy/MyMatches.java
+++ app/src/main/java/com/example/setpboardbuddy/MyMatches.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MyMatches extends AppCompatActivity {
 
@@ -12,10 +14,25 @@ public class MyMatches extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_matches);
+
+
+        //For Sort_By drop down list//
+        Spinner SortBy = (Spinner) findViewById(R.id.Sort_by);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MyMatches.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Sort_By_Itms));
+
+        //Giving drop down layout//
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SortBy.setAdapter(myAdapter);
     }
 
+    //For back button to function properly//
     public  void MainActivity(View view) {
         Intent MainActivity = new Intent(MyMatches.this, MainActivity.class);
         startActivity(MainActivity);
     }
+
+
+
 }
