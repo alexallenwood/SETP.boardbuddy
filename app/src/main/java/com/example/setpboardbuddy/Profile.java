1470febf24392c316f2ab40e068bc69be1c51537
Anjumol <up804697@myport.ac.uk>
2020-03-25 20:23:05 +0000

    back button to main activity added

--- app/src/main/java/com/example/setpboardbuddy/Profile.java
+++ app/src/main/java/com/example/setpboardbuddy/Profile.java
@@ -2,7 +2,9 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void BackBtn2(View view){
+        Intent myIntent = new Intent(Profile.this, MainActivity.class);
+        startActivity(myIntent);
+    }
+
+
 }
