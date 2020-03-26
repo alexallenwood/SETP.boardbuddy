6bdd651d03119c7a7b6882a867a9e8180e04e410
up899044 <61065304+up899044@users.noreply.github.com>
2020-03-26 21:25:50 +0000

    setting layout, adding create event and message button.

--- app/src/main/java/com/example/setpboardbuddy/GroupEvents.java
+++ app/src/main/java/com/example/setpboardbuddy/GroupEvents.java
@@ -2,7 +2,9 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GroupEvents extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class GroupEvents extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_events);
     }
+    public  void Back(View view) {
+        Intent MainActivity = new Intent(GroupEvents.this, MainActivity.class);
+        startActivity(MainActivity);
+    }
+
 }
