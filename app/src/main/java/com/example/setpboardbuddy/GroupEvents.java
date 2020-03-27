be2419d5ac6bb7ce776d6c8f69b5f2e16cc30fbd
up899044 <61065304+up899044@users.noreply.github.com>
2020-03-27 00:07:58 +0000

    Editing the sort by.

--- app/src/main/java/com/example/setpboardbuddy/GroupEvents.java
+++ app/src/main/java/com/example/setpboardbuddy/GroupEvents.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GroupEvents extends AppCompatActivity {
 
@@ -12,6 +14,16 @@ public class GroupEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_events);
+
+        //For Sort_By drop down list//
+        Spinner SortByGE = (Spinner) findViewById(R.id.sort_byGE);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(GroupEvents.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Sort_By_Events));
+
+        //Giving drop down layout//
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SortByGE.setAdapter(myAdapter);
     }
     public  void Back(View view) {
         Intent MainActivity = new Intent(GroupEvents.this, MainActivity.class);
